5cc11764187bc8f3d4271a7b9ffe4694b7fe83be
Artem Kasyanov <mr.revize.com@gmail.com>
2025-03-28 21:23:03 +0300

    smarthome 0.4.1:

--- telemetry/analyzer/src/main/resources/schema.sql
+++ telemetry/analyzer/src/main/resources/schema.sql
@@ -31,18 +31,16 @@ CREATE TABLE IF NOT EXISTS actions (
 -- связывающую сценарий, датчик и условие активации сценария
 CREATE TABLE IF NOT EXISTS scenario_conditions (
     scenario_id BIGINT REFERENCES scenarios(id),
-    sensor_id VARCHAR REFERENCES sensors(id),
     condition_id BIGINT REFERENCES conditions(id),
-    PRIMARY KEY (scenario_id, sensor_id, condition_id)
+    PRIMARY KEY (scenario_id, condition_id)
 );
 
 -- создаём таблицу scenario_actions,
 -- связывающую сценарий, датчик и действие, которое нужно выполнить при активации сценария
 CREATE TABLE IF NOT EXISTS scenario_actions (
     scenario_id BIGINT REFERENCES scenarios(id),
-    sensor_id VARCHAR REFERENCES sensors(id),
     action_id BIGINT REFERENCES actions(id),
-    PRIMARY KEY (scenario_id, sensor_id, action_id)
+    PRIMARY KEY (scenario_id, action_id)
 );
 
 -- создаём функцию для проверки, что связываемые сценарий и датчик работают с одним и тем же хабом
